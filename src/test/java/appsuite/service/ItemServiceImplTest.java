7842609a3b57e53d69a63c93f8aefb64f7392515
jaspal dhaliwal <jaspal dhaliwal>
2016-11-13 15:01:54 -0600

    POM Changes-JUnit

---
+++ src/test/java/appsuite/service/ItemServiceImplTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package appsuite.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author jaspal
+ *
+ */
+public class ItemServiceImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link appsuite.service.ItemServiceImpl#getItems()}.
+	 */
+	@Test
+	public final void testGetItems() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link appsuite.service.ItemServiceImpl#getItem(java.lang.String)}.
+	 */
+	@Test
+	public final void testGetItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link appsuite.service.ItemServiceImpl#deleteItem(java.lang.String)}.
+	 */
+	@Test
+	public final void testDeleteItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
