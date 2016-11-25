5160525fdc37d94aabd6053b00510b568d8d0717
jaspal dhaliwal <jaspal dhaliwal>
2016-11-24 18:08:50 -0600

    Refactoring done on some classes's package location

--- src/main/java/appsuite/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package appsuite.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import appsuite.domain.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Integer>{
-	
-}
